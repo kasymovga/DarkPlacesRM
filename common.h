bdea27a3ec0e7708581ea05baec2730d6faeb6e0
Micah Talkiewicz <mjtalkiewicz@gmail.com>
2016-02-21 17:56:50 -0500

    Added Vecxis to the list "IS_NEXUIZ_DERIVED".

--- common.h
+++ common.h
@@ -304,7 +304,7 @@ typedef enum gamemode_e
 gamemode_t;
 
 // Master switch for some hacks/changes that eventually should become cvars.
-#define IS_NEXUIZ_DERIVED(g) ((g) == GAME_NEXUIZ || (g) == GAME_XONOTIC || (g) == GAME_VORETOURNAMENT)
+#define IS_NEXUIZ_DERIVED(g) ((g) == GAME_NEXUIZ || (g) == GAME_XONOTIC || (g) == GAME_VORETOURNAMENT  || (g) == GAME_VECXIS)
 // Pre-csqcmodels era.
 #define IS_OLDNEXUIZ_DERIVED(g) ((g) == GAME_NEXUIZ || (g) == GAME_VORETOURNAMENT || (g) == GAME_VECXIS)
 
