54fc248c6e931606bf77c9d4d97af00bbb725ab9
Grigorii Kasymov <kasymovga@gmail.com>
2020-03-02 18:14:52 +0500

    improved rules for emscripten build

--- quakedef.h
+++ quakedef.h
@@ -446,10 +446,6 @@ extern cvar_t sessionid;
 # define DP_OS_NAME		"Web"
 # define DP_OS_STR		"Web"
 # define USE_GLES2		1
-# define LINK_TO_ZLIB	1
-# define LINK_TO_LIBPNG 1
-# define LINK_TO_LIBJPEG 1
-# define LINK_TO_LIBVORBIS 1
 # define DP_FREETYPE_STATIC 1
 #elif defined(__linux__)
 # define DP_OS_NAME		"Linux"
