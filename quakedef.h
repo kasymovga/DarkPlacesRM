2c71cde5e983852b37b6a4aac02f900f17e91967
Grigorii Kasymov <kasymovga@gmail.com>
2021-03-04 12:48:45 +0500

    decreased MAX_PARTICLES

--- quakedef.h
+++ quakedef.h
@@ -185,9 +185,9 @@ extern char engineversion[128];
 #define	PAINTBUFFER_SIZE		2048
 #define	MAX_BINDMAPS			8
 #define	MAX_PARTICLES_INITIAL	8192 ///< initial allocation for cl.particles
-#define	MAX_PARTICLES			1048576 ///< upper limit on cl.particles size
+#define	MAX_PARTICLES			32768 ///< upper limit on cl.particles size
 #define	MAX_DECALS_INITIAL		8192 ///< initial allocation for cl.decals
-#define	MAX_DECALS				1048576 ///< upper limit on cl.decals size
+#define	MAX_DECALS				32768 ///< upper limit on cl.decals size
 #define	MAX_ENITIES_INITIAL		256 ///< initial size of cl.entities
 #define	MAX_STATICENTITIES		1024 ///< limit on size of cl.static_entities
 #define	MAX_EFFECTS				256 ///< limit on size of cl.effects
