2b2e4aaee3d1bce4b95de22a15255be2d5e081ac
Grigorii Kasymov <kasymovga@gmail.com>
2022-12-03 18:55:11 +0500

    better check for fbo use

--- gl_rmain.c
+++ gl_rmain.c
@@ -4780,7 +4780,7 @@ static void R_Water_StartFrame(void)
 	int i;
 	int waterwidth, waterheight, texturewidth, textureheight, camerawidth, cameraheight;
 	r_waterstate_waterplane_t *p;
-	qboolean usewaterfbo = (r_viewfbo.integer >= 1 || r_water_fbo.integer >= 1) && vid.support.ext_framebuffer_object && vid.support.arb_texture_non_power_of_two && vid.samples < 2;
+	qboolean usewaterfbo = (r_viewfbo.integer >= 1 || r_water_fbo.integer >= 1) && vid.support.ext_framebuffer_object && (vid.samples < 2 || r_viewscale.value != 1 || r_water_resolutionmultiplier.value != 1);
 
 	if (vid.width > (int)vid.maxtexturesize_2d || vid.height > (int)vid.maxtexturesize_2d)
 		return;
@@ -4984,7 +4984,7 @@ static void R_Water_ProcessPlanes(int fbo, rtexture_t *depthtexture, rtexture_t 
 	int planeindex;
 	r_waterstate_waterplane_t *p;
 	vec3_t visorigin;
-	qboolean usewaterfbo = (r_viewfbo.integer >= 1 || r_water_fbo.integer >= 1) && vid.support.ext_framebuffer_object && vid.support.arb_texture_non_power_of_two && vid.samples < 2;
+	qboolean usewaterfbo = (r_viewfbo.integer >= 1 || r_water_fbo.integer >= 1) && vid.support.ext_framebuffer_object && (vid.samples < 2 || r_viewscale.value != 1 || r_water_resolutionmultiplier.value != 1);
 	char vabuf[1024];
 
 	originalview = r_refdef.view;
@@ -5220,7 +5220,7 @@ static void R_Bloom_StartFrame(void)
 	int i;
 	int bloomtexturewidth, bloomtextureheight, screentexturewidth, screentextureheight;
 	int viewwidth, viewheight;
-	qboolean useviewfbo = r_viewfbo.integer >= 1 && vid.support.ext_framebuffer_object && vid.support.arb_texture_non_power_of_two && vid.samples < 2;
+	qboolean useviewfbo = r_viewfbo.integer >= 1 && vid.support.ext_framebuffer_object && (vid.samples < 2 || r_viewscale.value != 1);
 	textype_t textype = TEXTYPE_COLORBUFFER;
 
 	switch (vid.renderpath)
