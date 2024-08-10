62988d1563036eabf35d4be86a3b36f40660ca4d
Grigorii Kasymov <kasymovga@gmail.com>
2024-08-10 10:37:31 +0500

    removed deprecated cvar

--- model_brush.c
+++ model_brush.c
@@ -62,7 +62,6 @@ cvar_t mod_q3shader_force_terrain_alphaflag = {0, "mod_q3shader_force_terrain_al
 
 cvar_t mod_q1bsp_polygoncollisions = {0, "mod_q1bsp_polygoncollisions", "0", "disables use of precomputed cliphulls and instead collides with polygons (uses Bounding Interval Hierarchy optimizations)"};
 cvar_t mod_collision_bih = {0, "mod_collision_bih", "1", "enables use of generated Bounding Interval Hierarchy tree instead of compiled bsp tree in collision code"};
-cvar_t mod_recalculatenodeboxes = {0, "mod_recalculatenodeboxes", "1", "enables use of generated node bounding boxes based on BSP tree portal reconstruction, rather than the node boxes supplied by the map compiler"};
 
 static texture_t mod_q1bsp_texture_solid;
 static texture_t mod_q1bsp_texture_sky;
@@ -106,7 +105,6 @@ void Mod_BrushInit(void)
 	Cvar_RegisterVariable(&mod_q3shader_force_terrain_alphaflag);
 	Cvar_RegisterVariable(&mod_q1bsp_polygoncollisions);
 	Cvar_RegisterVariable(&mod_collision_bih);
-	Cvar_RegisterVariable(&mod_recalculatenodeboxes);
 
 	// these games were made for older DP engines and are no longer
 	// maintained; use this hack to show their textures properly
